ca5c8b0f9f16d5885139a3071eda18c7e64bb33b
KyleJemino <kcjemino@up.edu.ph>
2023-07-29 23:21:31 +0800

    use dart_sass

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,12 @@ config :guimbal_waterworks, GuimbalWaterworksWeb.Endpoint,
   secret_key_base: "TELBEoxf7WeglI5Ad1ElZoAarfT2yDeKqfJhhCphg3PnJ3J4Hvk5LnconfX5ggml",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    sass: {
+      DartSass,
+      :install_and_run,
+      [:default, ~w(--embed-source-map --source-map-urls=absolute --watch)]
+    }
   ]
 
 # ## SSL Support
