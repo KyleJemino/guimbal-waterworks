1f0dea75381b0f857d74d01e700fe84d01f5c327
KyleJemino <kcjemino@up.edu.ph>
2023-07-30 14:32:03 +0800

    removes unneeded functions

--- lib/guimbal_waterworks/accounts/users_token.ex
+++ lib/guimbal_waterworks/accounts/users_token.ex
@@ -77,10 +77,6 @@ defmodule GuimbalWaterworks.Accounts.UsersToken do
   Users can easily adapt the existing code to provide other types of delivery methods,
   for example, by phone numbers.
   """
-  def build_email_token(users, context) do
-    build_hashed_token(users, context, users.email)
-  end
-
   defp build_hashed_token(users, context, sent_to) do
     token = :crypto.strong_rand_bytes(@rand_size)
     hashed_token = :crypto.hash(@hash_algorithm, token)
@@ -94,70 +90,8 @@ defmodule GuimbalWaterworks.Accounts.UsersToken do
      }}
   end
 
-  @doc """
-  Checks if the token is valid and returns its underlying lookup query.
-
-  The query returns the users found by the token, if any.
-
-  The given token is valid if it matches its hashed counterpart in the
-  database and the user email has not changed. This function also checks
-  if the token is being used within a certain period, depending on the
-  context. The default contexts supported by this function are either
-  "confirm", for account confirmation emails, and "reset_password",
-  for resetting the password. For verifying requests to change the email,
-  see `verify_change_email_token_query/2`.
-  """
-  def verify_email_token_query(token, context) do
-    case Base.url_decode64(token, padding: false) do
-      {:ok, decoded_token} ->
-        hashed_token = :crypto.hash(@hash_algorithm, decoded_token)
-        days = days_for_context(context)
-
-        query =
-          from token in token_and_context_query(hashed_token, context),
-            join: users in assoc(token, :users),
-            where: token.inserted_at > ago(^days, "day") and token.sent_to == users.email,
-            select: users
-
-        {:ok, query}
-
-      :error ->
-        :error
-    end
-  end
-
   defp days_for_context("confirm"), do: @confirm_validity_in_days
   defp days_for_context("reset_password"), do: @reset_password_validity_in_days
-
-  @doc """
-  Checks if the token is valid and returns its underlying lookup query.
-
-  The query returns the users found by the token, if any.
-
-  This is used to validate requests to change the users
-  email. It is different from `verify_email_token_query/2` precisely because
-  `verify_email_token_query/2` validates the email has not changed, which is
-  the starting point by this function.
-
-  The given token is valid if it matches its hashed counterpart in the
-  database and if it has not expired (after @change_email_validity_in_days).
-  The context must always start with "change:".
-  """
-  def verify_change_email_token_query(token, "change:" <> _ = context) do
-    case Base.url_decode64(token, padding: false) do
-      {:ok, decoded_token} ->
-        hashed_token = :crypto.hash(@hash_algorithm, decoded_token)
-
-        query =
-          from token in token_and_context_query(hashed_token, context),
-            where: token.inserted_at > ago(@change_email_validity_in_days, "day")
-
-        {:ok, query}
-
-      :error ->
-        :error
-    end
-  end
 
   @doc """
   Returns the token struct for the given token value and context.
