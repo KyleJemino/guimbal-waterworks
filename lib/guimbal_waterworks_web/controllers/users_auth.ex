c50f6832f2d6864bcd98d4eb39749c557433a4ef
KyleJemino <kcjemino@up.edu.ph>
2023-07-31 22:01:29 +0800

    logout redirect to home page

--- lib/guimbal_waterworks_web/controllers/users_auth.ex
+++ lib/guimbal_waterworks_web/controllers/users_auth.ex
@@ -81,7 +81,7 @@ defmodule GuimbalWaterworksWeb.UsersAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: "/users/log_in")
+    |> redirect(to: "/")
   end
 
   @doc """
