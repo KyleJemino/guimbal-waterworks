4ff6fba1f0f1813f056c4cdb102dd2e3ea145da4
KyleJemino <kcjemino@up.edu.ph>
2023-08-03 11:15:00 +0800

    use uuid for app

--- lib/guimbal_waterworks/accounts/users.ex
+++ lib/guimbal_waterworks/accounts/users.ex
@@ -2,6 +2,8 @@ defmodule GuimbalWaterworks.Accounts.Users do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "users" do
     field :username, :string
     field :first_name, :string
