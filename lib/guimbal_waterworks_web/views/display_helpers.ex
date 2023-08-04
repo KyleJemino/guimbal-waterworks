eab403ff7174afbb12856759bb010b5ef6fd2288
KyleJemino <kcjemino@up.edu.ph>
2023-08-04 15:39:27 +0800

    displays name in one column

--- lib/guimbal_waterworks_web/views/display_helpers.ex
+++ lib/guimbal_waterworks_web/views/display_helpers.ex
@@ -1,5 +1,6 @@
 defmodule GuimbalWaterworksWeb.DisplayHelpers do
   alias GuimbalWaterworks.Accounts.Users
+  alias GuimbalWaterworks.Members.Member
 
   def full_name(%Users{
         first_name: first_name,
@@ -7,5 +8,17 @@ defmodule GuimbalWaterworksWeb.DisplayHelpers do
         last_name: last_name
       }) do
     "#{last_name}, #{first_name}#{if not is_nil(middle_name), do: ", #{middle_name}"}"
+  end
+
+  def full_name(%Member{
+    first_name: first_name,
+    middle_name: middle_name,
+    last_name: last_name,
+    unique_identifier: identifier
+  }) do
+    formatted_name =
+      "#{last_name}, #{first_name}#{if not is_nil(middle_name), do: ", #{middle_name}"}"
+
+    "#{formatted_name}#{if not is_nil(identifier), do: " (#{identifier})"}"
   end
 end
