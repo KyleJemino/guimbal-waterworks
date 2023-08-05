cc30a63fe021f7942d9ed76005fa8cacc562ea4b
KyleJemino <kcjemino@up.edu.ph>
2023-08-06 02:20:08 +0800

    runs format

--- priv/repo/migrations/20230805175504_fix_uniq_meter_idx.exs
+++ priv/repo/migrations/20230805175504_fix_uniq_meter_idx.exs
@@ -5,10 +5,10 @@ defmodule GuimbalWaterworks.Repo.Migrations.FixUniqMeterIdx do
     drop unique_index(:members, [:meter_no])
 
     create unique_index(
-      :members, 
-      [:meter_no], 
-      where: "archived_at is null",
-      name: :uniq_active_meter_no_idx
-    )
+             :members,
+             [:meter_no],
+             where: "archived_at is null",
+             name: :uniq_active_meter_no_idx
+           )
   end
 end
