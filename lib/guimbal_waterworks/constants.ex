384a24786134e684dfe25c766d90414e8be46f9b
KyleJemino <kcjemino@up.edu.ph>
2023-08-08 22:36:52 +0800

    makes months field a select field

---
+++ lib/guimbal_waterworks/constants.ex
@@ -0,0 +1,12 @@
+defmodule GuimbalWaterworks.Constants do
+  def months do
+    Enum.map(
+      1..12, 
+      fn x -> 
+        x
+        |> Timex.month_name()
+        |> String.upcase()
+      end
+    )
+  end
+end
