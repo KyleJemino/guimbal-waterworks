0d6609ac0928c20bf1e5d1b73b03f71382f1b8e1
KyleJemino <kcjemino@up.edu.ph>
2023-08-12 22:22:43 +0800

    adds schema

--- priv/repo/migrations/20230812130309_create_bills_table.exs
+++ priv/repo/migrations/20230812130309_create_bills_table.exs
@@ -2,12 +2,12 @@ defmodule GuimbalWaterworks.Repo.Migrations.CreateBillsTable do
   use Ecto.Migration
 
   def change do
-    create table(:bills, primary_key:false) do
-      add :id, :binary_id, :primary_key: true
+    create table(:bills, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :reading, :integer, null: false
-      add :membership_fee, :boolean, null: false
-      add :adv_fee, :boolean, null: false
-      add :reconnection_fee, :boolean, null: false
+      add :membership_fee?, :boolean, null: false
+      add :adv_fee?, :boolean, null: false
+      add :reconnection_fee?, :boolean, null: false
       add :member_id, references(:members, type: :binary_id)
       add :billing_period_id, references(:billing_periods, type: :binary_id)
       add :user_id, references(:users, type: :binary_id)
