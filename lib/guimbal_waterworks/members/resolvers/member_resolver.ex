503572b1ca3e9c3bd02476e23d32c9ee1dd35939
KyleJemino <kcjemino@up.edu.ph>
2023-08-15 21:02:14 +0800

    moves list_member to MemberResolver

--- lib/guimbal_waterworks/members/resolvers/member_resolver.ex
+++ lib/guimbal_waterworks/members/resolvers/member_resolver.ex
@@ -1,6 +1,15 @@
 defmodule GuimbalWaterworks.Members.Resolvers.MemberResolver do
   alias GuimbalWaterworks.Repo
+
   alias GuimbalWaterworks.Members.Member
+  alias GuimbalWaterworks.Members.Queries.MemberQuery, as: MQ
+
+  def list_members(params \\ %{}) do
+    params
+    |> Map.put_new("with_archived?", false)
+    |> MQ.query_member()
+    |> Repo.all()
+  end
 
   def archive_member(member) do
     member
