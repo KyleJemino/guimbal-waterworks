cb5717df3436af2b6508ca5097eed86397fa4ab1
KyleJemino <kcjemino@up.edu.ph>
2023-08-23 20:08:01 +0800

    puts basic queries usage below custom queries

--- lib/guimbal_waterworks/bills/queries/bill_query.ex
+++ lib/guimbal_waterworks/bills/queries/bill_query.ex
@@ -6,8 +6,6 @@ defmodule GuimbalWaterworks.Bills.Queries.BillQuery do
     query_by(Bill, params)
   end
 
-  use GuimbalWaterworks, :basic_queries
-
   defp query_by(query, %{"member_id" => member_id} = params) do
     query
     |> where([q], q.member_id == ^member_id)
@@ -19,6 +17,8 @@ defmodule GuimbalWaterworks.Bills.Queries.BillQuery do
     |> where([q], is_nil(q.payment_id))
     |> query_by(Map.delete(params, "status"))
   end
+
+  use GuimbalWaterworks, :basic_queries
 
   use GuimbalWaterworks, :catch_query
 end
