40b127fdb0bb6f019ea35b48f032cab9b0523acf
KyleJemino <kcjemino@up.edu.ph>
2023-09-02 14:42:57 +0800

    adds streets constants

--- lib/guimbal_waterworks/constants.ex
+++ lib/guimbal_waterworks/constants.ex
@@ -1,6 +1,5 @@
 defmodule GuimbalWaterworks.Constants do
-  def months do
-    Enum.map(
+  @months Enum.map(
       1..12,
       fn x ->
         x
@@ -8,5 +7,40 @@ defmodule GuimbalWaterworks.Constants do
         |> String.upcase()
       end
     )
-  end
+
+  @streets [
+    "BALANTAD ST. (1)",
+    "BALANTAD ST. (2)",
+    "BALANTAD ST. (3)",
+    "BLUMENTRITT ST.",
+    "BURGOS ST.",
+    "C.COLON ST.",
+    "C.FRUTO ST.",
+    "GARBANZOS ST.",
+    "GARGARITANO ST.",
+    "GARIEL ST.",
+    "GENEROSA ST.",
+    "GENGOS ST.",
+    "GERONA ST.",
+    "GIMENO ST.",
+    "GIRADO ST.",
+    "GONZALES ST.",
+    "GOTERA ST.",
+    "GRANADA EXT.",
+    "GRANADA ST.",
+    "IGCOCOLO EXT. (1)",
+    "IGCOCOLO EXT. (2)",
+    "IGCOCOLO MAIN",
+    "LIBO-ON ST.",
+    "MAGSAYSAY ST.",
+    "PESCADORES ST.",
+    "PUBLIC MARKET",
+    "RIZAL ST.",
+    "RIZAL-TUGUISAN ST.",
+    "SPECIAL BILLING",
+    "TORREBLANCA ST."
+  ]
+
+  def months, do: @months
+  def streets, do: @streets
 end
