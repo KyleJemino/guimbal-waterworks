9c1f83f5a03ef2eb3aacd57712d1d32d2c3eed6f
KyleJemino <kcjemino@up.edu.ph>
2023-09-09 14:43:52 +0800

    adds total row

--- lib/guimbal_waterworks_web/live/bill_live/bill_list.ex
+++ lib/guimbal_waterworks_web/live/bill_live/bill_list.ex
@@ -2,17 +2,28 @@ defmodule GuimbalWaterworksWeb.BillLive.BillList do
   use GuimbalWaterworksWeb, :live_component
 
   alias GuimbalWaterworks.Bills
+  alias Decimal, as: D
+
+  @init_calculation_map %{
+    base_amount: 0,
+    franchise_tax_amount: 0,
+    adv_amount: 0,
+    membership_amount: 0,
+    reconnection_amount: 0,
+    surcharge: 0,
+    total: 0
+  }
 
   def update(assigns, socket) do
     {:ok,
       socket
       |> assign(assigns)
       |> assign(:base_params, assigns.base_params || %{})
-      |> assign_bills()
+      |> update_results()
     }
   end
 
-  defp assign_bills_total(socket) do
+  defp assign_bills_with_calculation(socket) do
     list_params =
       socket.assigns.base_params
       |> Map.put("preload", [:billing_period, :member, :payment])
@@ -35,5 +46,28 @@ defmodule GuimbalWaterworksWeb.BillLive.BillList do
       :bills,
       bills_with_calculation
     )
+  end
+
+  defp assign_total_calculation(%{assigns: %{ bills: bills }} = socket) do
+    total_price_map = Enum.reduce(bills, @init_calculation_map,
+        fn %{ calculation: calculation }, acc ->
+          Enum.reduce(acc, acc, fn 
+            {key, value}, acc ->
+              Map.update!(acc, key, fn current -> D.add(current, Map.fetch!(calculation, key)) end)
+          end)
+        end
+      )
+
+    assign(
+      socket,
+      :total_prices,
+      total_price_map
+    )
+  end
+
+  defp update_results(socket) do
+    socket
+    |> assign_bills_with_calculation()
+    |> assign_total_calculation()
   end
 end
