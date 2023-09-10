f5dcc713a37c2e7c09184f7095a86cfe14f1fcea
KyleJemino <kcjemino@up.edu.ph>
2023-09-10 20:07:04 +0800

    adds map of payments

--- lib/guimbal_waterworks_web/live/payment_live/payment_list.ex
+++ lib/guimbal_waterworks_web/live/payment_live/payment_list.ex
@@ -26,23 +26,42 @@ defmodule GuimbalWaterworksWeb.PaymentLive.PaymentList do
     assign(socket, :payments, payments)
   end
 
-  # defp assign_payment_bill_map(socket) do
-  #   {payment_bill_map, total_amount} =
-  #     Enum.reduce(
-  #       socket.assigns.payments, {%{}, 0}, fn
-  #         payment, { payment_bill_map_acc, payment_total_acc } ->
-  #           {bill_map, total_payment_acc} =
-  #             Enum.reduce(payment.bills, {%{}, 0}, 
-  #               fn bill, {bill_map_acc, payment_amount}->
-  #                    
-  #               end
-  #             ) 
-  #       end
-  #     )
-  # end
+  defp assign_payment_calculations(socket) do
+    {payment_bill_map, payment_total_map, total_amount} =
+      Enum.reduce(
+        socket.assigns.payments, {%{}, %{}, 0}, fn
+          payment, { payment_bill_map_acc, payment_total_map_acc, total_acc } ->
+            {bills_map, bills_total} =
+              Enum.reduce(payment.bills, {%{}, 0}, 
+                fn bill, {bill_map_acc, payment_amount}->
+                  bill_total = Bills.get_bill_total(bill)
+                  bill_map =
+                    Map.put(
+                      bill_map_acc, 
+                      Display.display_period(bill.billing_period),
+                      bill_total
+                    )
+                  {bill_map, D.add(payment_amount, bill_total)}
+                end
+              )
+            {
+              Map.put(payment_bill_map_acc, payment.id, bills_map),
+              Map.put(payment_total_map_acc, payment.id, bills_total),
+              D.add(total_acc, bills_total)
+            }
+        end
+      )
+
+    assign(socket, %{
+      payment_bill_map: payment_bill_map,
+      payment_total_map: payment_total_map,
+      total: total_amount
+    })
+  end
 
   defp update_results(socket) do
     socket
     |> assign_payments()
+    |> assign_payment_calculations()
   end
 end
