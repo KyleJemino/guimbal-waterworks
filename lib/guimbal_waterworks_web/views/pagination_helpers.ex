2018663019af4bef0ff1c479e2c3a60640e16443
KyleJemino <kcjemino@up.edu.ph>
2023-09-13 23:16:10 +0800

    fix pages_count calculation

--- lib/guimbal_waterworks_web/views/pagination_helpers.ex
+++ lib/guimbal_waterworks_web/views/pagination_helpers.ex
@@ -19,4 +19,54 @@ defmodule GuimbalWaterworksWeb.PaginationHelpers do
       %{}
     end
   end
+
+  def get_pagination_info(pagination_params, result_count, display_count) do
+    %{
+      "per_page" => per_page,
+      "current_page" => current_page
+    } = pagination_params
+
+    pages_count =
+      if per_page != "All" and result_count != 0 do
+        ceil(result_count / per_page)
+      else
+        1
+      end
+
+    pagination_chunks =
+      cond do
+        per_page == "All" ->
+          []
+
+        pages_count < 10 ->
+          [
+            Enum.to_list(1..pages_count)
+          ]
+
+        current_page < 7 ->
+          [
+            Enum.to_list(1..10),
+            [pages_count - 1, pages_count]
+          ]
+
+        current_page > pages_count - 6 ->
+          [
+            [1, 2],
+            Enum.to_list((pages_count - 9)..pages_count)
+          ]
+
+        true ->
+          [
+            [1],
+            Enum.to_list((current_page - 4)..(current_page + 4)),
+            [pages_count]
+          ]
+      end
+    %{
+      total_count: result_count,
+      display_count: display_count,
+      pages_count: pages_count,
+      pagination_chunks: pagination_chunks
+    }
+  end
 end
