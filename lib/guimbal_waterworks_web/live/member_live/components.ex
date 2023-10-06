8b9ce49077e19b7b518e5f3e637dd364a8130b21
KyleJemino <kcjemino@up.edu.ph>
2023-10-06 23:02:15 +0800

    adds header

--- lib/guimbal_waterworks_web/live/member_live/components.ex
+++ lib/guimbal_waterworks_web/live/member_live/components.ex
@@ -3,7 +3,15 @@ defmodule GuimbalWaterworksWeb.MemberLive.Components do
 
   def bill_card(assigns) do
     ~H"""
-      <div class="flex flex-row gap-4">
+      <div class="bill-card">
+        <div class="bill-header">
+          <img src={@bill_logo_src} class="logo"/>
+          <div class="header-text">
+            <p>Guimbal BWP-Rural Waterworks and Sanitation Association</p> 
+            <p>Poblacion, Guimbal</p>
+            <p class="contact">Contact #: 09778039982 / (033) 517-4642</p>
+          </div>
+        </div>
         <p><%= Display.full_name(@member) %></p>
         <p><%= Display.money(@bill_map.total) %></p>
       </div>
