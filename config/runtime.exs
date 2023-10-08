40e6b1e6bb72290508236e72f320a41369b80509
KyleJemino <kcjemino@up.edu.ph>
2023-10-08 14:52:12 +0800

    add heroku configuration for deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :guimbal_waterworks, GuimbalWaterworks.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
@@ -52,7 +52,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :guimbal_waterworks, GuimbalWaterworksWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: "guimbalwater.herokuapp.com", port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
