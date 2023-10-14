9f0e8f24371c15165ef54d5630448a67db1d55be
KyleJemino <kcjemino@up.edu.ph>
2023-10-14 21:39:27 +0800

    fix config

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,6 +53,7 @@ if config_env() == :prod do
 
   config :guimbal_waterworks, GuimbalWaterworksWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    force_ssl: [rewrite_on: [:x_forwarded_proto]],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
