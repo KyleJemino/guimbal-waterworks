538b5037e9084f890e09b1e5964911e908456a91
KyleJemino <kcjemino@up.edu.ph>
2023-10-15 23:04:21 +0800

    adds functionality to user role form

--- lib/guimbal_waterworks_web/live/employee_live/role_form_component.ex
+++ lib/guimbal_waterworks_web/live/employee_live/role_form_component.ex
@@ -9,7 +9,27 @@ defmodule GuimbalWaterworksWeb.EmployeeLive.RoleFormComponent do
     {:ok,
       socket
       |> assign(assigns)
+      |> assign(:employee, employee)
       |> assign(:changeset, changeset)
     }
+  end
+
+  def handle_event("validate", %{"users" => user_params}, socket) do
+    changeset = Accounts.change_user_role(socket.assigns.employee, user_params)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("save", %{"users" => user_params}, socket) do
+    case Accounts.update_user_role(socket.assigns.employee, user_params) do
+      {:ok, _user} ->
+        {:noreply,
+          socket
+          |> put_flash(:info, "Changed role successfully")
+          |> push_redirect(to: socket.assigns.return_to)
+        }
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
