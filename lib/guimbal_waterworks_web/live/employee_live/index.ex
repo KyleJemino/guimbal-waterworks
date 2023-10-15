7903296a17d942f3945e47c59ac1ae53fbe7f54d
KyleJemino <kcjemino@up.edu.ph>
2023-10-15 23:14:41 +0800

    fix index

--- lib/guimbal_waterworks_web/live/employee_live/index.ex
+++ lib/guimbal_waterworks_web/live/employee_live/index.ex
@@ -25,7 +25,9 @@ defmodule GuimbalWaterworksWeb.EmployeeLive.Index do
     case Accounts.get_users!(employee_id) do
       %Users{role: :manager, id: employee_id} 
       when employee_id != socket.assigns.current_users.id ->
-        push_patch(socket, to: Routes.employee_index_path(@socket, :index))
+        socket
+        |> put_flash(:error, "Invalid action")
+        |> push_patch(to: Routes.employee_index_path(socket, :index))
       %Users{} = employee ->
         socket
         |> assign(:page_title, "Edit Employee Role")
