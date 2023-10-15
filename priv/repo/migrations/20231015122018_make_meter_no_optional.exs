d348c61bcd78ba5049eccd9cf7fa3ddd4cd87b7a
KyleJemino <kcjemino@up.edu.ph>
2023-10-15 21:02:08 +0800

    runs mix format

--- priv/repo/migrations/20231015122018_make_meter_no_optional.exs
+++ priv/repo/migrations/20231015122018_make_meter_no_optional.exs
@@ -3,30 +3,30 @@ defmodule GuimbalWaterworks.Repo.Migrations.MakeMeterNoOptional do
 
   def up do
     drop_if_exists unique_index(
-      :members,
-      [:meter_no],
-      where: "archived_at is null",
-      name: :uniq_active_meter_no_idx
-    )
+                     :members,
+                     [:meter_no],
+                     where: "archived_at is null",
+                     name: :uniq_active_meter_no_idx
+                   )
 
     alter table(:members) do
       modify :meter_no, :string, null: true, from: :integer
     end
 
     create unique_index(
-      :members,
-      [:meter_no],
-      where: "archived_at is null",
-      name: :members_meter_no_unique_idx
-    )
+             :members,
+             [:meter_no],
+             where: "archived_at is null",
+             name: :members_meter_no_unique_idx
+           )
   end
 
   def down do
     drop unique_index(
-      :members,
-      [:meter_no],
-      where: "archived_at is null",
-      name: :members_meter_no_unique_idx
-    )
+           :members,
+           [:meter_no],
+           where: "archived_at is null",
+           name: :members_meter_no_unique_idx
+         )
   end
 end
