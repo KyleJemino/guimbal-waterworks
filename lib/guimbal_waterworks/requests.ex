279b52a90d6bb69a8148c457658213d29cbbc449
KyleJemino <kcjemino@up.edu.ph>
2023-10-23 04:35:18 +0800

    fix requests

--- lib/guimbal_waterworks/requests.ex
+++ lib/guimbal_waterworks/requests.ex
@@ -1,5 +1,5 @@
 defmodule GuimbalWaterworks.Requests do
   alias GuimbalWaterworks.Requests.Resolvers.RequestResolver, as: RR
 
-  defdelegate request_password_changeset(request, attrs), to: RR
+  defdelegate password_request_changeset(request, attrs), to: RR
 end
