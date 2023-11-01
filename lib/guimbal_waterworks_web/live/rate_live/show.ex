2732745810fcc9e276c0e60a46e8dd004e7fb909
KyleJemino <kcjemino@up.edu.ph>
2023-11-01 22:09:58 +0800

    adds rate show live view

--- lib/guimbal_waterworks_web/live/rate_live/show.ex
+++ lib/guimbal_waterworks_web/live/rate_live/show.ex
@@ -1,6 +1,8 @@
 defmodule GuimbalWaterworksWeb.RateLive.Show do
   use GuimbalWaterworksWeb, :live_view
 
+  alias GuimbalWaterworks.Bills
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -8,7 +10,21 @@ defmodule GuimbalWaterworksWeb.RateLive.Show do
 
   @impl true
   def handle_params(%{"id" => rate_id}, _url, socket) do
-    IO.inspect rate_id
-    {:noreply, socket}
+    rate = Bills.get_rate(%{"id" => rate_id})
+
+    sorted_rates =
+      Enum.sort(rate.usage_rates,
+        fn first, last ->
+          {usage1, _rates} = first
+          {usage2, _rates} = last
+
+          String.to_integer(usage1) <= String.to_integer(usage2)
+        end
+      )
+
+    {:noreply, assign(socket, %{
+      rate: rate,
+      sorted_rates: sorted_rates
+    })}
   end
 end
