fcf4a62e1c744ac7be5892436aa250e095acb18a
KyleJemino <kcjemino@up.edu.ph>
2023-11-01 19:25:26 +0800

    makes usage rate a map

--- lib/guimbal_waterworks/bills/rate.ex
+++ lib/guimbal_waterworks/bills/rate.ex
@@ -10,11 +10,7 @@ defmodule GuimbalWaterworks.Bills.Rate do
     field :membership_fee, :decimal
     field :surcharge_fee, :decimal
     field :tax_rate, :decimal
-
-    embeds_many :usage_rates, UsageRate, on_replace: :delete do
-      field :reading, :integer
-      field :reading_rate, :decimal
-    end
+    field :usage_rates, :map
 
     timestamps()
   end
@@ -26,21 +22,16 @@ defmodule GuimbalWaterworks.Bills.Rate do
       :surcharge_fee,
       :membership_fee,
       :tax_rate,
-      :title
+      :title,
+      :usage_rates
     ])
     |> validate_required([
       :reconnection_fee,
       :surcharge_fee,
       :membership_fee,
       :tax_rate,
-      :title
+      :title,
+      :usage_rates
     ])
-    |> cast_embed(:usage_rates, with: &usage_rate_changeset/2)
-  end
-
-  def usage_rate_changeset(usage_rate, attrs) do
-    usage_rate
-    |> cast(attrs, [:reading, :reading_rate])
-    |> validate_required([:reading, :reading_rate])
   end
 end
