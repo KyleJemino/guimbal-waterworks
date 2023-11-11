9c6ab087fd12c70f0970422bd799df55e8fe2193
KyleJemino <kcjemino@up.edu.ph>
2023-11-12 01:16:40 +0800

    runs format

--- lib/guimbal_waterworks_web/live/member_live/list_component.ex
+++ lib/guimbal_waterworks_web/live/member_live/list_component.ex
@@ -21,7 +21,7 @@ defmodule GuimbalWaterworksWeb.MemberLive.ListComponent do
     "last_name" => "",
     "unique_identifier" => "",
     "street" => "",
-    "type" => "all",
+    "type" => "all"
   }
 
   @valid_filter_keys [
