4bec68b58111fb5e3c82bd6ed0671ba04a5fd259
KyleJemino <kcjemino@up.edu.ph>
2023-11-12 23:51:35 +0800

    adds bill creation

--- lib/guimbal_waterworks_web/live/billing_period_live/show.ex
+++ lib/guimbal_waterworks_web/live/billing_period_live/show.ex
@@ -2,6 +2,7 @@ defmodule GuimbalWaterworksWeb.BillingPeriodLive.Show do
   use GuimbalWaterworksWeb, :live_view
 
   alias GuimbalWaterworks.Bills
+  alias GuimbalWaterworks.Members
 
   @impl true
   def mount(_params, _session, socket) do
@@ -15,8 +16,30 @@ defmodule GuimbalWaterworksWeb.BillingPeriodLive.Show do
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:billing_period, Bills.get_billing_period!(id))
      |> assign(:filter_params, params)
-     |> assign(:clean_params, Map.drop(params, ["id"]))
+     |> assign(:clean_params, Map.drop(params, ["id", "member_id"]))
+     |> assign_member(params)
+     |> assign_bill()
      |> assign_return_to()}
+  end
+
+  def handle_info({:create_member_bill, member_id}, socket) do
+    %{
+      clean_params: clean_params,
+      billing_period: billing_period
+    } = socket.assigns
+
+    create_bill_path =
+      Routes.billing_period_show_path(
+        socket, 
+        :new_bill, 
+        billing_period, 
+        member_id,
+        clean_params
+      )
+
+    {:noreply,
+      push_patch(socket, to: create_bill_path)
+    }
   end
 
   defp page_title(:edit), do: "Edit Billing period"
@@ -34,5 +57,28 @@ defmodule GuimbalWaterworksWeb.BillingPeriodLive.Show do
       )
 
     assign(socket, :return_to, return_to)
+  end
+
+  defp assign_member(socket, params) do
+    with {:ok, member_id} <- Map.fetch(params, "member_id"),
+         member <- Members.get_member!(member_id) do
+      assign(socket, :member, member)
+    else
+      _catch ->
+        assign(socket, :member, nil)
+    end
+  end
+
+  defp assign_bill(socket)do
+    case Map.fetch(socket.assigns, :member) do
+      {:ok, member} when not is_nil(member) ->
+        bill = Bills.new_bill(%{
+          member_id: member.id,
+          user_id: socket.assigns.current_users.id
+        })
+        assign(socket, :bill, bill)
+      _else ->
+        assign(socket, :bill, nil)
+    end
   end
 end
