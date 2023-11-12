2a17ba3c3eccf6a8a4845f6c99cbb85c695da672
KyleJemino <kcjemino@up.edu.ph>
2023-11-12 21:04:32 +0800

    fix form_redirects

--- lib/guimbal_waterworks_web/live/billing_period_live/show.ex
+++ lib/guimbal_waterworks_web/live/billing_period_live/show.ex
@@ -14,9 +14,24 @@ defmodule GuimbalWaterworksWeb.BillingPeriodLive.Show do
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:billing_period, Bills.get_billing_period!(id))
-     |> assign(:filter_params, params)}
+     |> assign(:filter_params, params)
+     |> assign(:clean_params, Map.drop(params, ["id"]))
+     |> assign_return_to()}
   end
 
   defp page_title(:edit), do: "Edit Billing period"
   defp page_title(_), do: "Show Billing period"
+
+  defp assign_return_to(socket) do
+    params = socket.assigns.filter_params
+    return_to = 
+      Routes.billing_period_show_path(
+        socket, 
+        :show,
+        Map.get(params, "id"),
+        socket.assigns.clean_params
+      )
+
+    assign(socket, :return_to, return_to)
+  end
 end
