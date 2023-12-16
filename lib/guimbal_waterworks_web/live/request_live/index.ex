a592b63ba558701fe686b6cbb3b2a0eac033e189
KyleJemino <kcjemino@up.edu.ph>
2023-12-16 21:23:31 +0800

    adds placeholder approve/reject button

--- lib/guimbal_waterworks_web/live/request_live/index.ex
+++ lib/guimbal_waterworks_web/live/request_live/index.ex
@@ -2,17 +2,27 @@ defmodule GuimbalWaterworksWeb.RequestLive.Index do
   use GuimbalWaterworksWeb, :live_view
 
   alias GuimbalWaterworks.Requests
-  alias GuimbalWaterworks.Requests.Request
+  # alias GuimbalWaterworks.Requests.Request
 
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
 
-  def handle_params(params, _url, socket) do
+  def handle_params(_params, _url, socket) do
     {:noreply,
       socket
       |> assign_requests()
     }
+  end
+
+  def handle_event("approve", %{"request_id" => request_id}, socket) do
+    IO.inspect request_id
+    {:noreply, socket}
+  end
+
+  def handle_event("reject", params, socket) do
+    IO.inspect params
+    {:noreply, socket}
   end
 
   defp assign_requests(socket) do
