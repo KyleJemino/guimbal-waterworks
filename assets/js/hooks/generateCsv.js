93c5608d25789ba7a51145709efe6c13528f0534
KyleJemino <kcjemino@up.edu.ph>
2023-12-18 21:36:57 +0800

    formats bills for csv

--- assets/js/hooks/generateCsv.js
+++ assets/js/hooks/generateCsv.js
@@ -10,7 +10,18 @@ export default {
         filename: `payments_${today.toISOString()}`
       })
 
-      const csv = generateCsv(csvConfig)(data)
+      const formattedData = data.map((paymentData) => {
+        const formattedBills = paymentData.bills.reduce(
+          (formatted, bill) => `${formatted}\n${bill.name}:${bill.amount}`, ''
+        )
+
+        return {
+          ...paymentData,
+          bills: formattedBills
+        }
+      })
+
+      const csv = generateCsv(csvConfig)(formattedData)
 
       download(csvConfig)(csv)
     })
