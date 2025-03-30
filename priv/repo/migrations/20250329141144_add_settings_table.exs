85e298b610b88f0fb426776435ca26068deffe0e
KyleJemino <kcjemino@up.edu.ph>
2025-03-30 19:51:48 +0800

    fix setting migration

--- priv/repo/migrations/20250329141144_add_settings_table.exs
+++ priv/repo/migrations/20250329141144_add_settings_table.exs
@@ -2,8 +2,10 @@ defmodule GuimbalWaterworks.Repo.Migrations.AddSettingsTable do
   use Ecto.Migration
 
   def change do
-    add :id, :binary_id, primary_key: true
-    add :contact_number, :string, null: false
-    add :address, :string, null: false
+    create table(:setting, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :contact_number, :string, null: false
+      add :address, :string, null: false
+    end
   end
 end
