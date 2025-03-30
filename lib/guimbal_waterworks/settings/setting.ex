8dc851871fedc7287d76a8c6acdf0fac783c52bf
KyleJemino <kcjemino@up.edu.ph>
2025-03-30 19:50:14 +0800

    add setting changeset

--- lib/guimbal_waterworks/settings/setting.ex
+++ lib/guimbal_waterworks/settings/setting.ex
@@ -5,7 +5,14 @@ defmodule GuimbalWaterworks.Settings.Setting do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "setting" do
-    field :contact_number, :string, null: false
-    field :address, :string, null: false
+    field :contact_number, :string
+    field :address, :string
+  end
+
+  @doc false
+  def changeset(setting, attrs) do
+    setting
+    |> cast(attrs, [:contact_number, :address])
+    |> validate_required([:contact_number, :address])
   end
 end
