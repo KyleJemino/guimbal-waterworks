dfe943c92b0880ca3faad5e3408c4cbac698e39d
KyleJemino <kcjemino@up.edu.ph>
2025-04-01 19:51:51 +0800

    add setting context

---
+++ lib/guimbal_waterworks/settings/settings.ex
@@ -0,0 +1,19 @@
+defmodule GuimbalWaterworks.Settings do
+  alias GuimbalWaterworks.Repo
+  alias GuimbalWaterworks.Settings.Setting
+
+  def create_or_update_setting(setting, attrs) do
+    case Repo.one(Setting) do
+      %Setting{} = setting ->
+        setting
+        |> Setting.changeset(attrs)
+        |> Repo.update()
+      _ ->
+        %Setting{}
+        |> Setting.changeset(attrs)
+        |> Repo.insert()
+    end
+  end
+
+  def get_setting(), do: Repo.one(Setting)
+end
