b47b8d720a04e9b6593e1404028d725f3ec6c19a
KyleJemino <kcjemino@up.edu.ph>
2025-04-01 20:19:44 +0800

    adds settings context and tests

--- lib/guimbal_waterworks/settings/settings.ex
+++ lib/guimbal_waterworks/settings/settings.ex
@@ -2,7 +2,7 @@ defmodule GuimbalWaterworks.Settings do
   alias GuimbalWaterworks.Repo
   alias GuimbalWaterworks.Settings.Setting
 
-  def create_or_update_setting(setting, attrs) do
+  def create_or_update_setting(attrs) do
     case Repo.one(Setting) do
       %Setting{} = setting ->
         setting
